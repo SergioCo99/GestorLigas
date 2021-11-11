6e9227fb2574aabc2c421ddb7976be76aa1c1fad
SergioCo99 <sergiocogollos@opendeusto.es>
2021-11-11 20:37:06 +0100

    Primer commit

---
+++ gestion/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Deporte, Liga
+# Register your models here.
+admin.site.register(Deporte)
+admin.site.register(Liga)
---
+++ gestion/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+# Create your models here.
+
+class Deporte(models.Model):
+	nombre = models.CharField(max_length=50, null=False, primary_key=True)
+	descripcion = models.CharField(max_length=1000)
+	def __str__(self):
+		return self.nombre
+
+class Liga(models.Model):
+	nombre = models.CharField(max_length=50, null=False, primary_key=True)
+	descripcion = models.CharField(max_length=1000)
+	deporte = models.ForeignKey(Deporte, on_delete=models.CASCADE)
+	#imagen = models.ImageField(default= 'default.png', blank = True)
+	fecha_creacion = models.DateField()
+	#Usuario creador
+
+	def __str__(self):
+		return self.nombre
