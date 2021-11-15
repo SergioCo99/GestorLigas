c9343f292cfca16eeaaaee569f3c67f4658dec33
SergioCo99 <sergiocogollos@opendeusto.es>
2021-11-15 18:56:15 +0100

    Empezando con API

--- gestion/views.py
+++ gestion/views.py
@@ -1,5 +1,14 @@
 from django.http import HttpResponse, Http404
 from django.shortcuts import render, get_object_or_404, get_list_or_404
+import requests
 
 def index(request):
 	return render(request, 'index.html')
+
+def estadisticas(request):
+    response = requests.get('http://data.nba.net/10s/prod/v2/2021/teams.json')
+    data = response.json()
+    teams = data['league']['vegas']
+    context = {'teams': teams}
+    #print('Teams:', teams)
+    return render(request, 'estadisticas.html', context)
