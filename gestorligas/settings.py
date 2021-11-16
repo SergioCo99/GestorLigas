96e1f8b49329e0c20d136a08c3e8a5aa5c58e37a
SergioCo99 <sergiocogollos@opendeusto.es>
2021-11-16 10:23:18 +0100

    Cambio a MySQL + Tabla API

--- gestorligas/settings.py
+++ gestorligas/settings.py
@@ -76,9 +76,13 @@ WSGI_APPLICATION = 'gestorligas.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+            'ENGINE': 'django.db.backends.mysql',
+            'NAME': 'gestorLigasDB',
+            'USER': 'sergioco',
+            'PASSWORD': 'pass',
+            'HOST': 'localhost',
+            'PORT': '3306',
+        }
 }
 
 
