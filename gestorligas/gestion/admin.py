9dff2fc5bda9a856413b400766d056c1b392c2a5
SergioCo99 <sergiocogollos@gmail.com>
2021-11-28 15:37:10 +0100

    Formularios

---
+++ gestorligas/gestion/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+from .models import Deporte, Liga, Equipo
+# Register your models here.
+admin.site.register(Deporte)
+admin.site.register(Liga)
+admin.site.register(Equipo)
---
+++ gestorligas/gestion/forms.py
@@ -0,0 +1,31 @@
+from django import forms
+from .models import Equipo, Deporte, Liga
+
+class FormularioEquipo(forms.ModelForm):
+	nombre_equipo = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ejemplo: Real Madrid C.F'}))
+	descripcion = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ejemplo: Equipo que ha ganado 34 ligas'}))
+
+	class Meta: #Declaramos todos los datos asociados al formulario
+	
+		model = Equipo
+		fields = ['nombre_equipo' ,'descripcion','liga']
+		widgets = {'liga': forms.Select(attrs={'class':'form-control', 'placeholder': 'Selecciona la liga del equipo'})}
+
+class FormularioDeporte(forms.ModelForm):
+	nombre_deporte = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ejemplo: Baloncesto'}))
+	descripcion = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ejemplo: Deporte de 5vs5'}))
+
+	class Meta: #Declaramos todos los datos asociados al formulario
+	
+		model = Deporte
+		fields = ['nombre_deporte' ,'descripcion']
+
+class FormularioLiga(forms.ModelForm):
+	nombre_liga = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ejemplo: NBA'}))
+	descripcion = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ejemplo: Liga de baloncesto de Norteamerica'}))
+
+	class Meta: #Declaramos todos los datos asociados al formulario
+	
+		model = Liga
+		fields = ['nombre_liga' ,'descripcion', 'deporte']
+		widgets = {'Deporte': forms.Select(attrs={'class':'form-control', 'placeholder': 'Selecciona el deporte de la liga'})}
